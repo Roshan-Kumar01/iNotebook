24ec47ea49e4f13c570deb816bd990954fa95c5c
Roshan Kumar <roshankumarrtk81@gmail.com>
2023-10-18 21:03:09 +0530

    implemented useContext Hook API for Notes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
+import NoteState from './context/notes/NoteState';
 
 import {
   BrowserRouter as Router,
@@ -12,6 +13,7 @@ import {
 function App() {
   return (
     <>
+    <NoteState>
     <Router>
       <Navbar/>
       <Routes>
@@ -19,6 +21,7 @@ function App() {
             <Route exact path="/about" element={<About/>}/>
       </Routes>
       </Router>
+      </NoteState>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import noteContext from '../context/notes/noteContext'
 
 const About = () => {
+  const a = useContext(noteContext)
+  useEffect(() =>{
+    a.update()
+  },[])
   return (
-    <div>This is about</div>
+    <div>
+        This is About {a.state.name} and he is in class {a.state.class}
+    </div>
   )
 }
 
