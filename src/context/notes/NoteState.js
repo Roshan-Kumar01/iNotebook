7463f8d231a38f3a84f1002c4827d4d9993583f2
Roshan Kumar <roshankumarrtk81@gmail.com>
2023-10-19 09:59:23 +0530

    Notes and Noteitem added

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,9 +1,121 @@
 import React, { useState } from "react";
 import NoteContext from "./noteContext";
 
+
+
 const NoteState = (props) => {
+      const notesInitial = [
+          {
+            "_id": "652e3f061cf831f9a6acf7aa",
+            "user": "652e3dad6c9c7e225408f53f",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2023-10-17T08:00:06.427Z",
+            "__v": 0
+          },
+          {
+            "_id": "652e61631f2bac7834e9b2e2",
+            "user": "652e3dad6c9c7e225408f53f",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2023-10-17T10:26:43.475Z",
+            "__v": 0
+          },
+          {
+            "_id": "652e3f061cf831f9a6acf7aa",
+            "user": "652e3dad6c9c7e225408f53f",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2023-10-17T08:00:06.427Z",
+            "__v": 0
+          },
+          {
+            "_id": "652e61631f2bac7834e9b2e2",
+            "user": "652e3dad6c9c7e225408f53f",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2023-10-17T10:26:43.475Z",
+            "__v": 0
+          },
+          {
+            "_id": "652e3f061cf831f9a6acf7aa",
+            "user": "652e3dad6c9c7e225408f53f",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2023-10-17T08:00:06.427Z",
+            "__v": 0
+          },
+          {
+            "_id": "652e61631f2bac7834e9b2e2",
+            "user": "652e3dad6c9c7e225408f53f",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2023-10-17T10:26:43.475Z",
+            "__v": 0
+          },
+          {
+            "_id": "652e3f061cf831f9a6acf7aa",
+            "user": "652e3dad6c9c7e225408f53f",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2023-10-17T08:00:06.427Z",
+            "__v": 0
+          },
+          {
+            "_id": "652e61631f2bac7834e9b2e2",
+            "user": "652e3dad6c9c7e225408f53f",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2023-10-17T10:26:43.475Z",
+            "__v": 0
+          },{
+            "_id": "652e3f061cf831f9a6acf7aa",
+            "user": "652e3dad6c9c7e225408f53f",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2023-10-17T08:00:06.427Z",
+            "__v": 0
+          },
+          {
+            "_id": "652e61631f2bac7834e9b2e2",
+            "user": "652e3dad6c9c7e225408f53f",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2023-10-17T10:26:43.475Z",
+            "__v": 0
+          },
+          {
+            "_id": "652e3f061cf831f9a6acf7aa",
+            "user": "652e3dad6c9c7e225408f53f",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2023-10-17T08:00:06.427Z",
+            "__v": 0
+          },
+          {
+            "_id": "652e61631f2bac7834e9b2e2",
+            "user": "652e3dad6c9c7e225408f53f",
+            "title": "My Title",
+            "description": "Please wake up early",
+            "tag": "personal",
+            "date": "2023-10-17T10:26:43.475Z",
+            "__v": 0
+          }
+        ]
+    const[notes,setNotes] = useState(notesInitial);
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes,setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
